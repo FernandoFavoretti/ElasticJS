65e6d54f61790e177c27617c794e5203bc265cc3
fernandofvprado@gmail.com <fernandofvprado@gmail.com>
2016-10-31 11:50:19 -0200

    D3

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,17 @@
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
+            tmp = item.split("=");
+            if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
+}
+
+
 define(['scripts/d3.v3', 'scripts/elasticsearch'], function (d3, elasticsearch) {
     var client = new elasticsearch.Client();
 
@@ -9,89 +23,59 @@ define(['scripts/d3.v3', 'scripts/elasticsearch'], function (d3, elasticsearch) 
             // Begin query.
 
             query: {
-                // Boolean query for matching and excluding items.
-                match: {"DESCRICAO": "salada"}
-            }
-            // Aggregate on the results
+                bool:{
+                    // Boolean query for matching and excluding items.
+                    must : [
+                        {term: {"DESCRICAO": "coca"}},
+                        {term: {"DESCRICAO": "250"}}
+                    ],
+                    should: [
+                        {fuzzy: {"DESCRICAO": "pet"}}
+                    ]
 
-            // End query.
+                }
+            },
+
+            aggs:{
+                avg_grade: {avg:{ "field" : "VL_UNIT" } },
+                max_price: {max : { "field" : "VL_UNIT" } },
+                min_price: {min : { "field" : "VL_UNIT" } },
+                intraday_return: {sum : { "field" : "VL_UNIT" } },
+                year: {
+                    date_histogram: {
+                        "field":     "DATA_EMISSAO",
+                        "interval":  "week",
+                        "format": "yyyy-MM-dd"
+                    }
+                }
+
+            }
+
+
         }
+
     }).then(function (resp) {
 
-        //My Data
-        var results = resp['hits']['hits'].map(function(i){
-            return i['_source'];
-        });
-
-        // pega valores unitarios
-        //Lavel do grafico
-        var onlyvals = []
-        var vals = ["Valor"]
-
-        //Adiciona valores ao array
-        results.map(function (i) {
-            vals.push(i.VL_UNIT)
-            onlyvals.push(i.VL_UNIT)
-        });
 
 
-        //Labels
-        var labels = ["Descricao"]
-        results.map(function (i) {
-            labels.push(i.DESCRICAO)
-        });
-
-        //Dates
-        var dates = ["Data"]
-        results.map(function (i) {
-            var date = i.DATA_EMISSAO
-            var year =  date.substr(6, 8)
-            var withoutYear = date.substr(0, 5)
-            withoutYear += "/20"+year
-            dates.push(withoutYear)
-        });
-
-        //Medias
-        var media = 0
-        onlyvals.map(function (i) {
-            media += parseFloat(i)
-        });
-
-        media = Number((media/onlyvals.length).toFixed(2));
-
-        //max
-        var max = 0
-        onlyvals.map(function (i) {
-            if(parseFloat(i) > max ){
-                max = parseFloat(i)
-            }
-        });
-
-
-        //min
-        var min = max
-        onlyvals.map(function (i) {
-            if(parseFloat(i) < min ){
-                min = parseFloat(i)
-            }
-        });
+        //***********************************************************
+        //Distribuições
 
 
         var color = d3.scale.linear()
             .domain([min, media, max])
             .range(['#930F16', '#F0F0D0', '#228B22']);
 
-        //Mapeia resutlados do source
-        var results = resp['hits']['hits'].map(function (i) {
-            return i['_source'];
-        });
 
+        var media = resp.aggregations.avg_grade.value;
+        var max = resp.aggregations.max_price.value;
+        var min = resp.aggregations.min_price.value;
 
 
         data = [
-            {label:"Val. Médio", value:media},
-            {label:"Val. Máximo", value:max},
-            {label:"Val. Mínimo", value:min}
+            {label:"Val. Médio", value: Number(media).toFixed(2)},
+            {label:"Val. Máximo", value: Number(max).toFixed(2)},
+            {label:"Val. Mínimo", value: Number(min).toFixed(2)}
         ];
 
 
@@ -162,7 +146,7 @@ define(['scripts/d3.v3', 'scripts/elasticsearch'], function (d3, elasticsearch) 
             .attr("dy", ".35em") //vertical align middle
             .attr("text-anchor", "end")
             .text(function(d){
-                return (d.value+"%");
+                return ("R$ "+d.value);
             })
             .attr("x", function(d){
                 var width = this.getBBox().width;
@@ -174,24 +158,142 @@ define(['scripts/d3.v3', 'scripts/elasticsearch'], function (d3, elasticsearch) 
                 div.style("left", d3.event.pageX+10+"px");
                 div.style("top", d3.event.pageY-25+"px");
                 div.style("display", "inline-block");
-                div.html((d.label)+"<br>"+(d.value)+"%");
+                div.html((d.label)+"<br>"+"R$ "+(d.value));
             });
         bar
             .on("mouseout", function(d){
                 div.style("display", "none");
             });
 
-        svg.insert("g",":first-child")
-            .attr("class", "axisHorizontal")
-            .attr("transform", "translate(" + (margin + labelWidth) + ","+ (height - axisMargin - margin)+")")
+
+        //*******************************************************************
+        // -> End Distribuições
+
+
+
+
+
+        //**********************************************************************
+        // Acc Chart
+
+        var color = d3.scale.linear()
+            .domain([min, media, max])
+            .range(['#930F16', '#F0F0D0', '#228B22']);
+
+
+        var acc = resp.aggregations.intraday_return.value;
+        var count = resp.hits.total;
+
+
+        dataAcc = [
+            {label:Number(acc).toFixed(2), value: count}
+        ];
+
+
+        var div = d3.select("body").append("div").attr("class", "toolTip");
+
+        var axisMarginAcc = 20,
+            marginAcc = 40,
+            valueMarginAcc = 4,
+            widthAcc = parseInt(d3.select('#accChart').style('width'), 10),
+            heightAcc = parseInt(d3.select('#accChart').style('height'), 10),
+            barHeightAcc = (heightAcc-axisMarginAcc-marginAcc*2)* 0.4/dataAcc.length,
+            barPaddingAcc = (heightAcc-axisMarginAcc-marginAcc*2)*0.6/dataAcc.length,
+            dataAcc, barAcc, svgAcc, scaleAcc, xAxisAcc, labelWidthAcc = 0;
+
+
+        svgAcc = d3.select('#chartAcc')
+            .append("svg")
+            .attr("width", widthAcc)
+            .attr("height", heightAcc);
+
+
+        scaleAcc = d3.scale.linear()
+            .domain([0, count])
+            .range([0, widthAcc - marginAcc*2 - labelWidthAcc]);
+
+        var xAxisAcc = d3.svg.axis()
+            .scale((acc))
+            .orient("bottom")
+
+        var yAxisAcc = d3.svg.axis()
+            .scale(count)
+            .orient("left")
+            .ticks(10);
+
+
+        barAcc = svgAcc.selectAll("g")
+            .data(dataAcc)
+            .enter()
+            .append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
             .call(xAxis);
 
+        barAcc.attr("class", "bar")
+            .attr("cx",0)
+            .attr("transform", function(d, i) {
+                return "translate(" + marginAcc + "," + (i * (barHeightAcc + barPaddingAcc) + barPaddingAcc) + ")";
+            });
+
+        barAcc.append("text")
+            .attr("class", "label")
+            .attr("y", barHeightAcc / 2)
+            .attr("dy", ".35em") //vertical align middle
+            .text(function(d){
+                return d.label;
+            }).each(function() {
+            labelWidthAcc = Math.ceil(Math.max(labelWidthAcc, this.getBBox().width));
+        });
+
+
+        barAcc.append("rect")
+            .attr("transform", "translate("+labelWidthAcc+", 0)")
+            .attr("height", barHeightAcc)
+            .attr("width", 0)
+            .transition()
+            .duration(1500)
+            .delay(function(d,i){ return i*250})
+            .attr("width", function(d){
+                return scale(d.value);
+            });
+
+        barAcc.append("text")
+            .attr("class", "value")
+            .attr("y", barHeightAcc / 2)
+            .attr("dx", -valueMarginAcc + labelWidthAcc) //margin right
+            .attr("dy", ".35em") //vertical align middle
+            .attr("text-anchor", "end")
+            .text(function(d){
+                return ("R$ "+d.value);
+            })
+            .attr("x", function(d){
+                var width = this.getBBox().width;
+                return Math.max(widthAcc + valueMarginAcc, scale(d.value));
+            });
+
+        barAcc
+            .on("mousemove", function(d){
+                div.style("left", d3.event.pageX+10+"px");
+                div.style("top", d3.event.pageY-25+"px");
+                div.style("display", "inline-block");
+                div.html((d.label)+"<br>"+"R$ "+(d.value));
+            });
+        barAcc
+            .on("mouseout", function(d){
+                div.style("display", "none");
+            });
+
+        svgAcc.insert("g",":first-child")
+            .attr("class", "axisHorizontal")
+            .attr("transform", "translate(" + (marginAcc + labelWidthAcc) + ","+ (heightAcc - axisMarginAcc - marginAcc)+")")
+            .call(xAxisAcc);
 
 
 
     });
-
-    /**
-     * Created by favoretti on 28/10/16.
-     */
 });
+
+/**
+ * Created by favoretti on 28/10/16.
+ */
